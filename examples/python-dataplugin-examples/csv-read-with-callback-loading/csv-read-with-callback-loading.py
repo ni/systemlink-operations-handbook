64702423ca97508aacad0eb3149fe7b7ce1f4abe
jschumme <jan.schummers@ni.com>
2021-01-13 17:37:13 +0100

    Enforcing black linter rules

--- examples/python-dataplugin-examples/csv-read-with-callback-loading/csv-read-with-callback-loading.py
+++ examples/python-dataplugin-examples/csv-read-with-callback-loading/csv-read-with-callback-loading.py
@@ -21,50 +21,61 @@ class Plugin:
 
         ###
         # Possible data types:
-        # DataTypeChnFloat32, DataTypeChnFloat64, DataTypeChnString, DataTypeChnDate, DataTypeChnUInt8, DataTypeChnInt16, DataTypeChnInt32, DataTypeChnInt64
+        # DataTypeChnFloat32, DataTypeChnFloat64, DataTypeChnString,
+        # DataTypeChnDate, DataTypeChnUInt8, DataTypeChnInt16,
+        # DataTypeChnInt32, DataTypeChnInt64
         ###
 
         self.tdm_tree = {
             "author": "NI",
             "description": "Example file",
-            "groups": [{
-                "name": "Example",
-                "description": "The first group",
-                "time": datetime.datetime(2020, 2, 11, 15, 31, 59, 342380),
-                "channels": [{
-                    "name": self.channelNames[0],
-                    "description": "",
-                    "values": [],
-                    "info": "Time in seconds",
-                    "type": "DataTypeChnFloat64"
-                }, {
-                    "name": self.channelNames[1],
-                    "description": "",
-                    "values": [],
-                    "unit_string": "km/h",
-                    "type": "DataTypeChnFloat64"
-                }, {
-                    "name": self.channelNames[2],
-                    "description": "",
-                    "values": [],
-                    "type": "DataTypeChnFloat64"
-                }, {
-                    "name": self.channelNames[3],
-                    "description": "",
-                    "values": [],
-                    "type": "DataTypeChnFloat64"
-                }, {
-                    "name": self.channelNames[4],
-                    "description": "",
-                    "values": [],
-                    "type": "DataTypeChnFloat64"
-                }, {
-                    "name": self.channelNames[5],
-                    "description": "",
-                    "values": [],
-                    "type": "DataTypeChnString"
-                }]
-            }]
+            "groups": [
+                {
+                    "name": "Example",
+                    "description": "The first group",
+                    "time": datetime.datetime(2020, 2, 11, 15, 31, 59, 342380),
+                    "channels": [
+                        {
+                            "name": self.channelNames[0],
+                            "description": "",
+                            "values": [],
+                            "info": "Time in seconds",
+                            "type": "DataTypeChnFloat64",
+                        },
+                        {
+                            "name": self.channelNames[1],
+                            "description": "",
+                            "values": [],
+                            "unit_string": "km/h",
+                            "type": "DataTypeChnFloat64",
+                        },
+                        {
+                            "name": self.channelNames[2],
+                            "description": "",
+                            "values": [],
+                            "type": "DataTypeChnFloat64",
+                        },
+                        {
+                            "name": self.channelNames[3],
+                            "description": "",
+                            "values": [],
+                            "type": "DataTypeChnFloat64",
+                        },
+                        {
+                            "name": self.channelNames[4],
+                            "description": "",
+                            "values": [],
+                            "type": "DataTypeChnFloat64",
+                        },
+                        {
+                            "name": self.channelNames[5],
+                            "description": "",
+                            "values": [],
+                            "type": "DataTypeChnString",
+                        },
+                    ],
+                }
+            ],
         }
 
         return {Path(file_path).stem: self.tdm_tree}
@@ -83,16 +94,16 @@ class Plugin:
             else:
                 value = None
             values.append(value)
-        return values[numberToSkip:numberToTake+numberToSkip]
+        return values[numberToSkip : numberToTake + numberToSkip]
 
 
 if __name__ == "__main__":
-    print("To test your plugin first, run the Python file directly from the command line")
+    print(
+        "To test your plugin first, run the Python file directly from the command line"
+    )
     p = Plugin()
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    parameter = {
-        "file": dir_path + "\\Example.csv"
-    }
+    parameter = {"file": dir_path + "\\Example.csv"}
     print("\n %s" % p.read_store(parameter))
     print("\nChannel length: %s" % p.read_channel_length(0, 0))
     print("\nChannel values: %s" % p.read_channel_values(0, 0, 0, 1024))
